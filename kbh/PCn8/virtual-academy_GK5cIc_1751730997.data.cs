4996a9fca53f60995a4355e32db3fd1bf90fc457
rOG0025eR <keyed.hotspot_0t@icloud.com>
2025-07-05 16:56:40 +0100

    Improve test coverage for the file

---
+++ kbh/PCn8/virtual-academy_GK5cIc_1751730997.data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lbl.IdentityModel
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Data.Entity;
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public IDbSet<Resource> Resources { get; set; }
+
+        public IDbSet<Permission> Permissions { get; set; }
+    }
+}
