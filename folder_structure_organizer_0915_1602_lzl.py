fdf96dfeef24a7d741cdea2dc44a03925d1e10dd
rOG0025eR <keyed.hotspot_0t@icloud.com>
2025-09-15 09:02:13 +0100

    Add new 文件夹结构整理器 feature

---
+++ folder_structure_organizer_0915_1602_lzl.py
@@ -0,0 +1,10 @@
+# 代码生成时间: 2025-09-15 16:02:12
+import os
+import shutil
+from pathlib import Path
+
+class FolderStructureOrganizer:
+    """Class to organize the structure of a folder."""
+
+    def __init__(self, source_folder, target_folder):
+        "
